7b5b788c8cd2b2502c4168b2167de6baa76f9b21
Marcel Mulder <mmulder@victronenergy.com>
2014-03-05 14:49:50 +0100

    version: Textual changes

--- version.h
+++ version.h
@@ -7,13 +7,13 @@ const QString cVERSION = QString(VERSION);
 #define NAME "dbus_qwacs"
 const QString cNAME = QString(NAME);
 
-#define VE_PROD_NAME "Wireless AC Sensors"
+#define VE_PROD_NAME "PV Inverter"
 const QString cVE_PROD_NAME = QString(VE_PROD_NAME);
 
 #define VE_PROD_ID_QWACS "1234"
 const QString cVE_PROD_ID_QWACS = QString(VE_PROD_ID_QWACS);
 
-#define CONNECTION "Network"
+#define CONNECTION "Wireless AC sensors"
 const QString cCONNECTION = QString(CONNECTION);
 
 #endif // VERSION_H
