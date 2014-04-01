4fa0014a8499fc76239555e9459341d9d329e5ca
Marcel Mulder <mmulder@victronenergy.com>
2014-04-01 13:25:26 +0200

    updated product ID

--- version.h
+++ version.h
@@ -10,7 +10,7 @@ const QString cNAME = QString(NAME);
 #define VE_PROD_NAME "PV Inverter"
 const QString cVE_PROD_NAME = QString(VE_PROD_NAME);
 
-#define VE_PROD_ID_QWACS "0xA121"
+#define VE_PROD_ID_QWACS "0xA140"
 const QString cVE_PROD_ID_QWACS = QString(VE_PROD_ID_QWACS);
 
 #define CONNECTION "Wireless AC sensors"
