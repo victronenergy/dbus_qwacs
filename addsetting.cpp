a9757f9f9929da3688cecd570071cdb3247ebfea
Marcel Mulder <mmulder@victronenergy.com>
2014-08-20 10:27:36 +0200

    addsetting: removed

--- addsetting.cpp
+++
@@ -1,6 +0,0 @@
-#include "addsetting.h"
-
-AddSetting::AddSetting(const QString &service, const QString &path, const QDBusConnection &connection, QObject *parent) :
-	QDBusAbstractInterface(service, path, staticInterfaceName(), connection, parent)
-{
-}
